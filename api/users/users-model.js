bc49b1e91e9812276419ed8727e41d96ac8daf33
c_melchor <cah.melchor@gmail.com>
2021-01-28 12:08:56 -0800

    Created routers

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,7 +4,35 @@ module.exports = {
     getAll() {
         return db("users")
     },
-    // getById(id){
-    //     return db("users")
-    // }
+    getById(id) {
+        return db("users")
+            .where("id", id)
+            .first()
+    },
+    async insert(user) {
+        return db("users").insert(user)
+            .then(id => {
+                return db("users")
+                    .where("id", id)
+                    .first()
+            })
+    },
+    async update(changes, id) {
+        return db("users")
+            .where("id", id)
+            .update(changes)
+            .then(id => {
+                return db("users")
+                    .where("id", id)
+                    .first();
+            });
+    },
+    async remove(id) {
+        return db("users").where("id", id).del()
+            .then(id => {
+                return db("users")
+                    .where("id", id)
+                    .first()
+            })
+    }
 }
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -12,4 +12,26 @@ router.get("/", (req, res) => {
         });
 });
 
+router.post("/", (req, res) => {
+    Users.insert(req.body)
+        .then(newUser => {
+            res.status(201).json(newUser)
+        })
+        .catch(err => {
+            res.status(500).json({ message: err })
+        })
+});
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    Users.remove(id)
+        .then(deletedUser => {
+            res.status(200).json(deletedUser)
+        })
+        .catch(err => {
+            res.status(500).json({ message: err })
+        })
+
+});
+
 module.exports = router;
